98ec6c7c683392d983f43103fb4d51e3552876a4
Hommin <hommin84255@163.com>
2019-06-26 15:47:42 +0800

    5.5 添加QQ扫码登录方式 - 修复: QQ的get_user_info接口添加参数; jackson转换时忽略不存在的属性

--- hommin-security-core/src/main/java/com/hommin/security/core/social/SocialConfig.java
+++ hommin-security-core/src/main/java/com/hommin/security/core/social/SocialConfig.java
@@ -4,9 +4,15 @@ import com.hommin.security.core.properties.SecurityProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.encrypt.Encryptors;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurerAdapter;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
 import org.springframework.social.security.SpringSocialConfigurer;
+
+import javax.sql.DataSource;
 
 /**
  * @author Hommin
@@ -16,7 +22,15 @@ import org.springframework.social.security.SpringSocialConfigurer;
 public class SocialConfig extends SocialConfigurerAdapter {
 
 	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
 	private SecurityProperties securityProperties;
+
+	@Override
+	public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
+		return new JdbcUsersConnectionRepository(dataSource, connectionFactoryLocator, Encryptors.noOpText());
+	}
 
 	@Bean
 	public SpringSocialConfigurer homminSocialSecurityConfig() {
--- hommin-security-core/src/main/java/com/hommin/security/core/social/qq/api/QQImpl.java
+++ hommin-security-core/src/main/java/com/hommin/security/core/social/qq/api/QQImpl.java
@@ -2,49 +2,55 @@ package com.hommin.security.core.social.qq.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.javafx.binding.StringFormatter;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.social.oauth2.AbstractOAuth2ApiBinding;
 
 /**
- *
- *
  * @author Hommin
  * 2019年06月21日 5:04 PM
  */
-public class QQImpl extends AbstractOAuth2ApiBinding implements QQ{
+@Slf4j
+public class QQImpl extends AbstractOAuth2ApiBinding implements QQ {
 
     private final String appId;
 
     private final String openId;
 
+    private final String accessToken;
+
 
     private static final String URL_GET_OPENID = "https://graph.qq.com/oauth2.0/me?access_token=%s";
 
-    private static final String URL_GET_USERINFO = "https://graph.qq.com/user/get_user_info?oauth_consumer_key=%s&openid=%s";
+    private static final String URL_GET_USERINFO = "https://graph.qq.com/user/get_user_info?access_token=%s&oauth_consumer_key=%s&openid=%s";
 
     private static ObjectMapper objectMapper = new ObjectMapper();
 
     public QQImpl(String appId, String accessToken) {
         super(accessToken);
         this.appId = appId;
+        this.accessToken = accessToken;
         // 构造获取openId的url
         String openIdUrl = StringFormatter.format(URL_GET_OPENID, accessToken).getValue();
         // 请求openId
         String result = getRestTemplate().getForObject(openIdUrl, String.class);
         this.openId = StringUtils.substringBetween(result, "\"openid\":\"", "\"}");
-
     }
 
     @Override
     public QQUserInfo getUserInfo() {
         // 构造获取user info的url
-        String userInfoUrl = StringFormatter.format(URL_GET_USERINFO, appId, openId).getValue();
+        String userInfoUrl = String.format(URL_GET_USERINFO, accessToken, appId, openId);
         // 请求openId
         String userInfoStr = getRestTemplate().getForObject(userInfoUrl, String.class);
         QQUserInfo userInfo;
         try {
             userInfo = objectMapper.readValue(userInfoStr, QQUserInfo.class);
+            if ("-1".equals(userInfo.getRet())) {
+                throw new RuntimeException("获取用户信息失败");
+            }
             userInfo.setOpenId(openId);
+            log.info("QQ登录, 解析到user:{}", userInfo);
             return userInfo;
         } catch (Exception e) {
             throw new RuntimeException("获取用户信息失败", e);
