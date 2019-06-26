62d4083e4235b33caf86adac1571048570a91cf6
Hommin <hommin84255@163.com>
2019-06-26 16:25:27 +0800

    5.6 添加QQ注册 - 新用户扫描直接以第三方用户信息自动注册

--- hommin-security-core/src/main/java/com/hommin/security/core/social/SocialConfig.java
+++ hommin-security-core/src/main/java/com/hommin/security/core/social/SocialConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.encrypt.Encryptors;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurerAdapter;
 import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.ConnectionSignUp;
 import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
 import org.springframework.social.security.SpringSocialConfigurer;
@@ -27,9 +28,16 @@ public class SocialConfig extends SocialConfigurerAdapter {
 	@Autowired
 	private SecurityProperties securityProperties;
 
+	@Autowired(required = false)
+	private ConnectionSignUp connectionSignUp;
+
 	@Override
 	public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
-		return new JdbcUsersConnectionRepository(dataSource, connectionFactoryLocator, Encryptors.noOpText());
+		JdbcUsersConnectionRepository connectionRepository = new JdbcUsersConnectionRepository(dataSource, connectionFactoryLocator, Encryptors.noOpText());
+		if(connectionSignUp != null){
+			connectionRepository.setConnectionSignUp(connectionSignUp);
+		}
+		return connectionRepository;
 	}
 
 	@Bean
---
+++ hommin-security-demo/src/main/java/com/hommin/security/security/DemoConnectionSignUp.java
@@ -0,0 +1,20 @@
+package com.hommin.security.security;
+
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionSignUp;
+import org.springframework.stereotype.Component;
+
+/**
+ * 当第三方用户登录, 而该用户没有新建账号时, 添加新账号并认为其登录
+ *
+ * @author Hommin
+ * 2019年06月26日 4:13 PM
+ */
+@Component
+public class DemoConnectionSignUp implements ConnectionSignUp{
+
+    @Override
+    public String execute(Connection<?> connection) {
+        return connection.getKey().getProviderUserId();
+    }
+}
