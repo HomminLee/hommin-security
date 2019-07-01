a263f399c68a337ac500577198c9f799a1e58fb8
Hommin <hommin84255@163.com>
2019-07-01 15:36:23 +0800

    6.2 OAuth2简易资源服务器

---
+++ hommin-security-app/src/main/java/com/hommin/security/app/AppSecurityResourceConfig.java
@@ -0,0 +1,14 @@
+package com.hommin.security.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+/**
+ * @author Hommin
+ * 2019年06月28日 5:32 PM
+ */
+@Configuration
+@EnableResourceServer
+public class AppSecurityResourceConfig {
+
+}
